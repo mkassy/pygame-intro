833a88e8bb7c6fe1a57ede87834ec5af43c248b9
mkazzy <maha.kasem@protonmail.com>
2022-05-24 19:04:51 -0400

    Displayed score on game.

--- intro.py
+++ intro.py
@@ -1,6 +1,12 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = pygame.time.get_ticks() - start_time # gets time in milliseconds
+    score_surf = test_font.render(f'{current_time}', False,(64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf,score_rect) # surface, and then pos
+
 pygame.init()
 screen = pygame.display.set_mode((800,500))
 # screen_rect = screen.get_rect()
@@ -8,12 +14,13 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('pixeltype/Pixeltype.ttf', 50) # arguments: font type, font size
 game_active = True
+start_time = 0
 
 sky_surf = pygame.image.load('graphics/sky.png').convert() # convert alpha removes alpha values
 soil_surf = pygame.image.load('graphics/soil.jpg').convert() # convert makes game run faster
 
-score_surf = test_font.render('My game', False, (64,64,64)) # test, AA, color(RGB or hex_color)
-score_rect = score_surf.get_rect(center = (400,50))
+# score_surf = test_font.render('My game', False, (64,64,64)) # test, AA, color(RGB or hex_color)
+# score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surf = pygame.image.load('graphics/snail.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright = (800,400))
@@ -40,17 +47,21 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                player_rect.right = 0
+                start_time = pygame.time.get_ticks()
 
     if game_active:
         # draw all our elements   
         screen.blit(sky_surf, (0,0))
         screen.blit(soil_surf, (0,400))
-        pygame.draw.rect(screen,'#c0e8ec', score_rect) # takes 3+ arguments
-        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10) # 4th arg is line width, 5th is border rounding
+        # pygame.draw.rect(screen,'#c0e8ec', score_rect) # takes 3+ arguments
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect, 10) # 4th arg is line width, 5th is border rounding
         # pygame.draw.line(screen, 'gold', (0,0), pygame.mouse.get_pos(), width=5)
         # pygame.draw.ellipse(screen, 'brown', pygame.Rect(50,200,100,100)) # pygame.Rect() arguments: left, top, width, height
 
-        screen.blit(score_surf, score_rect)
+        # screen.blit(score_surf, score_rect)
+        display_score()
+
         snail_rect.x -= 4
         if snail_rect.right <= -100: snail_rect.left = 800
         screen.blit(snail_surf, snail_rect)
@@ -69,7 +80,10 @@ while True:
     else:
         screen.fill('Yellow')
         game_over = test_font.render('GAME OVER!!!', False, 'red')
+        press_keyfont = pygame.font.Font('pixeltype/Pixeltype.ttf', 30)
+        press_key = press_keyfont.render('Press space to continue.', False, 'orange')
         screen.blit(game_over,(300,200))
+        screen.blit(press_key,(270,400))
 
     # update everything
     pygame.display.update()
